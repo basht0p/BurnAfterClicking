58b0394cc8c8c33a7f87622a8591241810a2b228
basht0p <60065396+connor1337@users.noreply.github.com>
2022-05-03 21:12:33 -0400

    a

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/api/linkget',
     });
     db.run(killQuery)
 
-    console.log("Message viewed and burned by: " + req.socket.remoteaddress)
+    console.log("Message viewed and burned by: " + req.ip)
     });
 
 app.get('/show/',
